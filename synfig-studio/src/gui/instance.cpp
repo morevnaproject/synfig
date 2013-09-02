333647581dba840c2465f34dd2677a9878ad0193
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2013-09-02 19:57:29 +0700

    Don't display warning when saving with zip extension

--- synfig-studio/src/gui/instance.cpp
+++ synfig-studio/src/gui/instance.cpp
@@ -348,7 +348,7 @@ studio::Instance::dialog_save_as()
 		try
 		{
 			String ext(filename_extension(filename));
-			if(ext!=".sif" && ext!=".sifz" && !App::dialog_yes_no(_("Unknown extension"),
+			if(ext!=".sif" && ext!=".sifz" && ext!=".zip" && !App::dialog_yes_no(_("Unknown extension"),
 				_("You have given the file name an extension\nwhich I do not recognize. Are you sure this is what you want?")))
 				continue;
 		}
