morevnaproject/synfig

commit e08b97215b161150e2d7c42f32d9e550f6e01136

Fix 542 It is possible to link one ancestor bone to its children bone
- if selected_value_desc exist in paramlist and is bone then not candidate

synfig-studio/src/synfigapp/actions/valuedescbonelink.cpp
@@ -6,6 +6,7 @@
 **
 **	\legal
 **	......... ... 2013 Ivan Mahonin
+**	......... ... 2014 Jerome Blanchi
 **
 **	This package is free software; you can redistribute it and/or
 **	modify it under the terms of the GNU General Public License as
@@ -96,15 +97,29 @@ Action::ValueDescBoneLink::get_param_vocab()
 bool
 Action::ValueDescBoneLink::is_candidate(const ParamList &x)
 {
-	ParamList::const_iterator i;
+	ParamList::const_iterator iter;
 
 	ValueDesc value_desc(x.find("value_desc")->second.get_value_desc());
+
+	bool selected_value_desc_is_bone = false;
+	pair<ParamList::const_iterator, ParamList::const_iterator> selected_vd_range = x.equal_range("selected_value_desc");
+	//get the range of selected value desc to check if at least one is a bone
+	for (iter = selected_vd_range.first; iter != selected_vd_range.second ; ++iter)
+	{
+		ValueDesc selected_value_desc(iter->second.get_value_desc());
+		if (ValueNode_Bone::Handle::cast_dynamic(selected_value_desc.get_parent_value_node()))
+		{
+			selected_value_desc_is_bone = true;
+			break;
+		}
+	}
 
 	if (!candidate_check(get_param_vocab(),x))
 		return false;
 
 	return value_desc.parent_is_value_node()
-		&& ValueNode_Bone::Handle::cast_dynamic(value_desc.get_parent_value_node());
+		&& ValueNode_Bone::Handle::cast_dynamic(value_desc.get_parent_value_node())
+		&& !selected_value_desc_is_bone;
 }
 
 bool

commit 100e9b3041eb05983c824d1621f2a1bcd8bb1acc

Fix crash introduced by commit e08b97215b1611 (fix 542 link ancestor bone)
- Test 'parent_is_value_node' before 'get_parent_value_node' !
- TODO about improvement : "if bone, reject only if same skeleton"

synfig-studio/src/synfigapp/actions/valuedescbonelink.cpp
@@ -107,7 +107,11 @@ Action::ValueDescBoneLink::is_candidate(const ParamList &x)
 	for (iter = selected_vd_range.first; iter != selected_vd_range.second ; ++iter)
 	{
 		ValueDesc selected_value_desc(iter->second.get_value_desc());
-		if (ValueNode_Bone::Handle::cast_dynamic(selected_value_desc.get_parent_value_node()))
+		//Simple test of bone type
+		//TODO : if bone, reject only if same skeleton
+		if ((selected_value_desc.parent_is_value_node()) &&
+			(ValueNode_Bone::Handle::cast_dynamic(selected_value_desc.get_parent_value_node()))
+			)
 		{
 			selected_value_desc_is_bone = true;
 			break;